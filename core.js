a7adb03091a3dd01f5382143887a10a812b8e817
I-make-gamez <ebelcher143@gmail.com>
2021-11-11 18:40:51 +0000

    0.5.1

--- core.js
+++ core.js
@@ -1,21 +1,25 @@
+const t = document.getElementsByClassName("tl")[0];
+const c = document.getElementById("c");
+const l = document.getElementById("l");
+const yt = document.getElementById('yt');
+
 var year = -500000;
+var yeta = 0
 var bcad = "BC";
 
-const c = document.getElementById("#c");
-const l = document.getElementById("#l");
-const t = document.getElementsByClassName(".U1")[0];
+t.addEventListener("click", function() {
+    window.alert('T is defined!');
+}, false);
 
 c.addEventListener("click", function() {
     year += 1;
+    yeta += 1;
     if (year >= 1) {
         bcad = "AD";
     }
     if (year <= 1) {
         bcad = "BC";
     }
-    l.innerHTML = `Year: `
-});
-
-t.addEventListener("click", function() {
-    window.alert('T is defined!')
-});
+    l.innerHTML = `Year: ${Math.abs(year)} ${bcad}`;
+    yt.innerHTML = `Years Travelled: ${yeta}`
+}, false);
