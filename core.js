f7426f4704637cf513c432d76615c3daa579c4c0
I-make-gamez <ebelcher143@gmail.com>
2021-11-12 18:34:44 +0000

    0.6.6

--- core.js
+++ core.js
@@ -3,8 +3,10 @@ const le = document.getElementById('le');
 const c = document.getElementById("c");
 const l = document.getElementById("l");
 const yt = document.getElementById('yt');
-const vers
+const vers = document.getElementById('ver');
+const achive = document.getElementById('achive')
 
+var version = "0.6.6"
 var year = -500000;
 var yeta = 0;
 var bcad = "BC";
@@ -50,19 +52,31 @@ le.addEventListener('click', function() {
             le.style.opacity = 0
             setInterval(() => {
                 tl_timer -= 1
-                if(tl_timer <= 1){
+                if(tl_timer <= 1 && tl_open == false){
                     le.style.opacity = 1;
                     tl_open = true
                     return;
                 }else{
                     return;
                 };
-            }, 1300);
+            }, 1000);
             break;
     }
+}, false);
+
+achive.addEventListener('click', function(){
+    alert("Comming in 1.0.0")
+}, false);
+
+vers.addEventListener('click', function(){
+    alert("What's New?\n1) 'Time Leap' powerup added\n2) Bug Fixes")
 }, false);
 
 function unlock_tl(){
     t.style.opacity = 0;
     le.style.opacity = 1;
+}
+
+function updateVersion(){
+    vers.innerHTML = `Version: ${version}`
 }
